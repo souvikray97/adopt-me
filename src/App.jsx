ea64853ac5b32ac5ce3ee5838412fe875fcbd93a
souvik ray <souvikray97@gmail.com>
2024-05-22 15:51:21 +0530

    implimented a hook to SearchParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
-import ReactDOM from "react-dom";
-import Pet from "./Pet";  
+import ReactDOM from "react-dom/client";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
 
 const App = () => {
+  return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet animal="Dog" name="Luna" breed="Havanese" />
-      <Pet animal="Bird" name="Pepper" breed="Cockatiel" />
-      <Pet animal="Cat" name="Doink" breed="Mixed" />
+      <SearchParams /> 
     </div>
+  );
 };
   
 const container = document.getElementById("root"); 
